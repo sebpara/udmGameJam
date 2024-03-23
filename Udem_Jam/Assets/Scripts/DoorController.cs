a8409e484cc5edefee51f303eb62b737c8db5fe1
simonmenard1738 <poisonedcheeto@gmail.com>
2024-03-23 13:51:42 -0400

    Door logic done + question hall

--- Udem_Jam/Assets/Scripts/DoorController.cs
+++ Udem_Jam/Assets/Scripts/DoorController.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class DoorController : MonoBehaviour
 {
-    public Transform playerTransform;
-    public Transform cameraTrack;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +13,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        cameraTrack.position = new Vector3(playerTransform.position.x, playerTransform.position.y, cameraTrack.position.z);
+        
     }
-
 }
---
+++ Udem_Jam/Assets/Scripts/RoomController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool started;
+    public GameObject door;
+    void Start()
+    {
+        started = false;
+        //spawn enemies
+        started = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!enemyCheck() && started){
+            door.GetComponent<BoxCollider2D>().enabled = false;
+
+        }
+    }
+
+    bool enemyCheck(){
+        for(int i = 0; i < this.gameObject.transform.childCount; i++)
+        {
+            if(this.gameObject.transform.GetChild(i).tag=="Enemy"){
+                return true;
+            }
+        }
+        Debug.Log("opening door");
+        door.GetComponent<SpriteRenderer>().color = Color.blue;
+        return false;
+    }
+}
