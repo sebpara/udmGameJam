810d2b351a999aa47695c3c5d271b0eaff2c6659
simonmenard1738 <poisonedcheeto@gmail.com>
2024-03-24 11:08:41 -0400

    Portals now work

--- Udem_Jam/Assets/Scripts/PlayerController.cs
+++ Udem_Jam/Assets/Scripts/PlayerController.cs
@@ -7,10 +7,18 @@ public class PlayerController : MonoBehaviour
     public Camera SceneCamera;
     public float moveSpeed;
     public Rigidbody2D rb;
+    Vector3[] levels;
+    int index;
 
     private Vector2 moveDirection;
     private Vector2 mousePosition;
 
+    void Start(){
+        index = 0;
+        levels = new Vector3[2];
+        levels[0] = new Vector3(108.16f, 0.71f, 0f);
+        levels[1] = new Vector3(205.5f, 0.71f, 0f);
+    }
     void Update()
     {
         ProcessInputs();
@@ -35,5 +43,10 @@ public class PlayerController : MonoBehaviour
         Vector2 aimDirection = mousePosition - rb.position;
         float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = aimAngle;
+    }
+
+    public void LevelUp(){
+        this.GetComponent<Transform>().position = levels[index];
+        index++;
     }
 }
