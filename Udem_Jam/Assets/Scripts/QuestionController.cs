43e7b59f58b009a71243d308720f1e1648d1f13f
simonmenard1738 <poisonedcheeto@gmail.com>
2024-03-24 11:50:02 -0400

    Added questions

--- Udem_Jam/Assets/Scripts/QuestionController.cs
+++ Udem_Jam/Assets/Scripts/QuestionController.cs
@@ -34,7 +34,7 @@ public class QuestionController : MonoBehaviour
     }
 
     void instantiateQuestion(){
-        switch(Random.Range(0, 2)){
+        switch(Random.Range(0, 9)){
             case 0: 
                 this.quizzed = new Quiz("Identify a good buffer: \n\n A) a solution containing small amounts of both a weak acid and its conjugate base \n\n B) a solution containing significant amounts of both a strong acid and a strong base \n\n C) a solution containing significant amounts of both a weak acid and its conjugate base", 'C');
                 break;
@@ -51,6 +51,19 @@ public class QuestionController : MonoBehaviour
                 this.quizzed = new Quiz("Which of the following is TRUE? \n\n A) An effective buffer has a [conj base]/[weak acid] ratio in the range of 10 - 100. \n\n B) A buffer is most resistant to pH change when [acid] = [conjugate base]. \n\n C) An effective buffer has very small absolute concentrations of acid and conjugate base.", 'B');
                 break;
             case 5:
+                this.quizzed = new Quiz("Define buffer capacity. \n\n A) Buffer capacity is the amount of acid or base that can be added to a buffer without destroying its effectiveness. \n\n B) Buffer capacity is the amount of acid that can be added until all the base in the buffer is consumed. \n\n C) Buffer capacity is the amount of base that can be added until all the acid in the buffer is consumed.", 'A');
+                break;
+            case 6:
+                this.quizzed = new Quiz("Which of the following acids (listed with pKa values) and its conjugate base could form a buffer with a pH of 8.10? \n\n A) HC7H5O2, pKa = 4.19 \n\n B) HF, pKa = 3.46 \n\n C) HClO, pKa = 7.54", 'C');
+                break;
+            case 7:
+                this.quizzed = new Quiz("Which of the following acids (listed with Ka values) and their conjugate base would be the best choice to make a buffer with a pH of 2.34? \n\n A) C6H5COOH, Ka = 6.5 × 10-5 \n\n B) HClO2, Ka = 1.1 × 10-2 \n\n C) HClO, Ka = 2.9 × 10-8", 'B');
+                break;
+            case 8:
+                this.quizzed = new Quiz("Which one of the following statements is TRUE? \n\n A)	A buffer is an aqueous solution composed of two weak acids. \n\n B)	A buffer can absorb an unlimited amount of acid or base. \n\n C)   A buffer resists pH change by neutralizing added acids and bases.", 'C');
+                break;
+            case 9:
+                this.quizzed = new Quiz("Which of the following pairs of species is not a conjugate acid-base pair?  \n\n A) HCl and H+ \n\n B) HSO¬4- and SO42- C) NH3 and NH2-", 'C');
                 break;
         }
         activatePortal();
